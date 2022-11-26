b2a7c7c6d9d699b03fc851b5dd397310459de6fa
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-11-26 08:15:56 +0800

    Update fikker.sh

--- fikker.sh
+++ fikker.sh
@@ -1,4 +1,5 @@
 #！/bin/bash
+service iptables stop 2> /dev/null ; chkconfig iptables off 2> /dev/null ; service httpd stop 2> /dev/null ; service nginx stop 2> /dev/null ; chkconfig httpd off 2> /dev/null ; chkconfig nginx off 2> /dev/null ; systemctl stop firewalld.service 2> /dev/null ; systemctl disable firewalld.service 2> /dev/null ; systemctl stop httpd.service 2> /dev/null ; systemctl stop nginx.service 2> /dev/null ; systemctl disable httpd.service 2> /dev/null ; systemctl disable nginx.service 2> /dev/null ; if [ -f "/usr/bin/apt-get" ]; then apt-get install -y wget tar; fi ; if [ -f "/usr/bin/yum" ]; then yum install -y wget tar; fi ;
 mkdir fikker
 cd fikker
 wget http://xl.1syan.com/files/fikker.zip
