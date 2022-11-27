0e85f4753df439bcd09c54f574dcca7a41f17114
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-11-27 17:42:34 +0800

    Update fikker.sh

--- fikker.sh
+++ fikker.sh
@@ -1,7 +1,4 @@
 #！/bin/bash
-FikkerInstallDir="/root" # default installation directory
-FikkerNewVersion="fikkerd-3.7.6-linux-x86-64" # package name
-service iptables stop && chkconfig iptables off && service httpd stop && service nginx stop && chkconfig httpd off && chkconfig nginx off && systemctl stop firewalld.service && systemctl disable firewalld.service && systemctl stop httpd.service && systemctl stop nginx.service && systemctl disable httpd.service && systemctl disable nginx.service && yum -y install wget && cd $FikkerInstallDir && wget -c --no-check-certificate https://raw.githubusercontent.com/xu5343/fikker/main/$FikkerNewVersion.tar.gz && tar zxf $FikkerNewVersion.tar.gz && rm -rf $FikkerNewVersion.tar.gz && cd $FikkerNewVersion && ./fikkerd.sh install && ./fikkerd.sh start && cd $FikkerInstallDir && sleep 5 && echo 'finished!'
 echo "添加定时任务：每8小时重启fikker服务定时任务"
 # 判断文件夹是否存在
 if [ ! -e /var/spool/cron/ ];then
