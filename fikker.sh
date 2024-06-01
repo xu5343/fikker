6ce6a8cc996ee646aa89e831d744b9f0916fe5c1
xu5343 <32597757+xu5343@users.noreply.github.com>
2024-06-01 12:48:53 +0800

    Update fikker.sh

--- fikker.sh
+++ fikker.sh
@@ -1,6 +1,5 @@
-#！/bin/bash
+#!/bin/bash
 echo "添加定时任务：每7小时30分钟重启fikker服务定时任务"
-# 判断文件夹是否存在
 LOG_FILE="/root/fikkerd_restart.log"
 while true; do
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Restarting fikkerd service" | tee -a $LOG_FILE
@@ -12,7 +11,3 @@ while true; do
     fi
     sleep 27000  # 7小时30分钟 = 7*3600 + 30*60 秒
 done
-echo "/usr/bin/sh /usr/local/bin/fikker.sh &" >> /etc/rc.d/rc.local
-echo "计算器是否在运行" 
-ps aux | grep fikker.sh
-echo "登录 Fikker 管理后台：http://your-fikker-ip:6780/，管理员的初始密码：123456" 
